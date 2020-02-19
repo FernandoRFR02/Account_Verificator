fab13fa9bdd5d260356560e64c8e7c090a606af9
Rubinke <fernandorfr02@gmail.com>
2020-02-19 09:33:36 -0400

    With out verification

---
+++ Foro/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Foro.Models;
+
+namespace Foro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Resultado(Cliente clt)
+        {
+            
+            string cadena = clt.SummonerName;
+            cadena = cadena.Replace(" ","%20");
+            string region = "";
+            switch (clt.Region)
+            {
+                case "LAN":
+                    region = "la1";
+                    break;
+                case "RU":
+                    region = "ru";
+                    break;
+                case "KR":
+                    region = "kr";
+                    break;
+                case "BR":
+                    region = "br1";
+                    break;
+                case "JP":
+                    region = "jp1";
+                    break;
+                case "NA":
+                    region = "na1";
+                    break;
+                case "OC":
+                    region = "oc1";
+                    break;
+                case "EUN":
+                    region = "eun1";
+                    break;
+                case "EUW":
+                    region = "euw1";
+                    break;
+                case "TR":
+                    region = "tr1";
+                    break;
+                case "LAS":
+                    region = "la2";
+                    break;
+            }
+            ViewBag.urlReq = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + cadena + "?api_key=" + clt.ApiKey;
+            return View(clt);
+        }
+
+    }
+}
---
+++ Foro/Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Foro.Models
+{
+    public class Cliente
+    {
+        public string SummonerName { get; set; }
+        public string Region { get; set; }
+        public string ApiKey = "RGAPI-97ec8d3d-23b5-4e58-a59f-d6a6b4648e95";
+        public string genURL { get; set; }
+    }
+
+    public enum Regiones
+    {
+        NA,
+        EUW,
+        LAN,
+        LAS,
+        KR,
+        OC,
+        RU,
+        BR,
+        JP,
+        EUN,
+        TR
+    }
+}
